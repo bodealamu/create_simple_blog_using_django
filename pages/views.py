538f12b6a7537d57869f65a025ca8ba794b2d981
olabodealamu <bodealamu007@gmail.com>
2020-10-02 20:32:49 -0500

    added bootstrap styling

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from blog.models import Post
 
-# Create your views here.
+
+def home(request):
+    """View function for home page"""
+    published_posts = Post.objects.all().filter(published=True)
+
+    context = {
+        'published_posts':published_posts,
+    }
+
+    return render(request=request, template_name='pages/home.html', context=context)
